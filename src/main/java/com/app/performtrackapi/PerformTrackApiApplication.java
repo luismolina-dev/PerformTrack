0854f04ed83b611ce8971e03f18d85c97d14d2ae
luismolina-dev <luismolina.dev@gmail.com>
2026-02-06 11:46:44 -0600

    Initialize Spring Boot project with PostgreSQL, Gradle configuration, and Docker Compose setup.

---
+++ src/main/java/com/app/performtrackapi/PerformTrackApiApplication.java
@@ -0,0 +1,13 @@
+package com.app.performtrackapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformTrackApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerformTrackApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/performtrackapi/PerformTrackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.performtrackapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformTrackApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
